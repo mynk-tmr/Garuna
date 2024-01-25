63a27a0deb34fea4c33e149e3cc304c22a898419
Mayank <mynk.secure@gmail.com>
2024-01-26 02:40:13 +0530

    build: add tailwindcss

--- config/vite.config.js
+++ config/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,9 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [autoprefixer]
+      plugins: [tailwindcss({
+        config: './config/tailwind.config.js'
+      }), autoprefixer]
     }
   }
 })
--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,6 @@ import './app.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    Hello
+    <p className='text-green-400 text-[3rem]'>Hello</p>
   </React.StrictMode>,
 )
