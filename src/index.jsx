5879e4c930bbb48f7af96c21c5a40ed462f6f189
Mayank <mynk.secure@gmail.com>
2024-01-26 02:00:32 +0530

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <main style={{
+      background: 'url("https://miro.medium.com/v2/resize:fit:720/format:webp/1*o4ZajeN_bDPama0XfYFU7Q.jpeg")',
+      height: '100vh',
+    }}>
+    </main>
+  </React.StrictMode>,
+)
